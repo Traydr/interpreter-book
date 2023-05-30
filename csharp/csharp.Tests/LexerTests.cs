802c7fe682e608365dcd33216544ef35c96591ac
Traydr <Traydr@protonmail.com>
2023-05-30 22:06:39 +0300

    feat: lexer test

--- csharp/csharp.Tests/LexerTests.cs
+++ csharp/csharp.Tests/LexerTests.cs
@@ -10,6 +10,41 @@ public class Tests
     [Test]
     public void Test1()
     {
+        string input = "=+(){},;";
+
+        List<Token> tokens = new List<Token>()
+        {
+            new Token(TokenType.Assign, '='),
+            new Token(TokenType.Plus, '+'),
+            new Token(TokenType.Lparen, '('),
+            new Token(TokenType.Rparen, ')'),
+            new Token(TokenType.Lsquirly, '{'),
+            new Token(TokenType.Rsquirly, '}'),
+            new Token(TokenType.Comma, ','),
+            new Token(TokenType.Semicolon, ';'),
+            new Token(TokenType.Eof, ""),
+        };
+
+        Lexer lexer = new Lexer(input);
+
+        int index = 0;
+        foreach (var expected in tokens)
+        {
+            var token = lexer.NextToken();
+
+            if (token.Type != expected.Type)
+            {
+                Assert.Fail($"Tests[{index}] - TestTokenType Wrong. Expected:{expected.Type}, Got:{token.Type}");
+            }
+
+            if (token.Literal != expected.Literal)
+            {
+                Assert.Fail($"Tests[{index}] - Literal Wrong. Expected:{expected.Literal}, Got:{token.Literal}");
+            }
+
+            index++;
+        }
+
         Assert.Pass();
     }
 }
---
+++ csharp/csharp/Lexer.cs
@@ -0,0 +1,16 @@
+﻿namespace csharp;
+
+public class Lexer
+{
+    public readonly string _input;
+
+    public Lexer(string input)
+    {
+        _input = input;
+    }
+
+    public Token NextToken()
+    {
+        return new Token(TokenType.Illegal, "");
+    }
+}
