a7883591ae3dcbf72e707f8d1f3dc395cc992a5c
Traydr <Traydr@protonmail.com>
2023-05-31 22:52:11 +0300

    feat: add func to compare input to list of tokens

---
+++ csharp/csharp.Tests/TestUtils.cs
@@ -0,0 +1,27 @@
+﻿namespace csharp.Test;
+
+public class TestUtils
+{
+    public static void CompareExpectedTokens(List<Token> expected, string input)
+    {
+        Lexer lexer = new Lexer(input);
+
+        int index = 0;
+        foreach (var expect in expected)
+        {
+            var token = lexer.NextToken();
+
+            if (token.Type != expect.Type)
+            {
+                Assert.Fail($"Tests[{index}] - TestTokenType Wrong. Expected:{expect.Type}, Got:{token.Type}");
+            }
+
+            if (token.Literal != expect.Literal)
+            {
+                Assert.Fail($"Tests[{index}] - Literal Wrong. Expected:{expect.Literal}, Got:{token.Literal}");
+            }
+
+            index++;
+        }
+    }
+}
