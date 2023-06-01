ae6225e6e89255dac3861226106f101f6e516b53
Traydr <Traydr@protonmail.com>
2023-06-01 18:20:00 +0300

    feat: add test nextrokenv4

--- csharp/csharp.Tests/LexerTests.cs
+++ csharp/csharp.Tests/LexerTests.cs
@@ -30,7 +30,7 @@ public class Tests
     }
 
     [Test]
-    public void TestNextToken()
+    public void TestNextTokenV3()
     {
         string input = """
                             let five = 5;
@@ -80,6 +80,79 @@ public class Tests
             new Token(TokenType.Comma, ','),
             new Token(TokenType.Ident, "ten"),
             new Token(TokenType.Rparen, ')'),
+            new Token(TokenType.Semicolon, ';'),
+            new Token(TokenType.Eof, ""),
+        };
+
+        TestUtils.CompareExpectedTokens(tokens, input);
+        Assert.Pass();
+    }
+
+    [Test]
+    public void TestNextTokenV4()
+    {
+        string input = """
+                            let five = 5;
+                            let ten = 10;
+
+                            let add = fn(x, y) {
+                              x + y;
+                            };
+
+                            let result = add(five, ten);
+                            !-/*5;
+                            5 < 10 > 5;
+                       """;
+
+        List<Token> tokens = new List<Token>()
+        {
+            new Token(TokenType.Let, "let"),
+            new Token(TokenType.Ident, "five"),
+            new Token(TokenType.Assign, '='),
+            new Token(TokenType.Int, "5"),
+            new Token(TokenType.Semicolon, ';'),
+            new Token(TokenType.Let, "let"),
+            new Token(TokenType.Ident, "ten"),
+            new Token(TokenType.Assign, '='),
+            new Token(TokenType.Int, "10"),
+            new Token(TokenType.Semicolon, ';'),
+            new Token(TokenType.Let, "let"),
+            new Token(TokenType.Ident, "add"),
+            new Token(TokenType.Assign, '='),
+            new Token(TokenType.Function, "fn"),
+            new Token(TokenType.Lparen, '('),
+            new Token(TokenType.Ident, "x"),
+            new Token(TokenType.Comma, ','),
+            new Token(TokenType.Ident, "y"),
+            new Token(TokenType.Rparen, ')'),
+            new Token(TokenType.Lsquirly, '{'),
+            new Token(TokenType.Ident, "x"),
+            new Token(TokenType.Plus, '+'),
+            new Token(TokenType.Ident, "y"),
+            new Token(TokenType.Semicolon, ';'),
+            new Token(TokenType.Rsquirly, '}'),
+            new Token(TokenType.Semicolon, ';'),
+            new Token(TokenType.Let, "let"),
+            new Token(TokenType.Ident, "result"),
+            new Token(TokenType.Assign, '='),
+            new Token(TokenType.Ident, "add"),
+            new Token(TokenType.Lparen, '('),
+            new Token(TokenType.Ident, "five"),
+            new Token(TokenType.Comma, ','),
+            new Token(TokenType.Ident, "ten"),
+            new Token(TokenType.Rparen, ')'),
+            new Token(TokenType.Semicolon, ';'),
+            new Token(TokenType.Bang, '!'),
+            new Token(TokenType.Minus, '-'),
+            new Token(TokenType.ForwardSlash, '/'),
+            new Token(TokenType.Asterisk, '*'),
+            new Token(TokenType.Int, "5"),
+            new Token(TokenType.Semicolon, ';'),
+            new Token(TokenType.Int, "5"),
+            new Token(TokenType.LessThan, '<'),
+            new Token(TokenType.Int, "10"),
+            new Token(TokenType.GreaterThan, '>'),
+            new Token(TokenType.Int, "5"),
             new Token(TokenType.Semicolon, ';'),
             new Token(TokenType.Eof, ""),
         };
