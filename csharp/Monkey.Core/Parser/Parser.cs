bca490e1f0f461089dd783b4816d8c85e283849b
Traydr <Traydr@protonmail.com>
2023-06-08 12:13:13 +0300

    feat: begin parser

---
+++ csharp/Monkey.Core/Parser/Parser.cs
@@ -0,0 +1,29 @@
+﻿using Monkey.Core.Lexer;
+
+namespace Monkey.Core.Parser;
+
+public class Parser
+{
+    private Lexer.Lexer _lexer;
+    private Token _currentToken;
+    private Token _peekToken;
+
+    public Parser(string input)
+    {
+        _lexer = new Lexer.Lexer(input);
+
+        NextToken();
+        NextToken();
+    }
+
+    private void NextToken()
+    {
+        _currentToken = _peekToken;
+        _peekToken = _lexer.NextToken();
+    }
+
+    public Ast.Program ParseProgram()
+    {
+        return new Ast.Program(Array.Empty<Ast.Statement>());
+    }
+}
