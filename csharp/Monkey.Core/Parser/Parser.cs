84052cbe57dee540061e2c9a93c38e9649c4c949
Traydr <Traydr@protonmail.com>
2023-06-12 11:54:35 +0300

    docs: parser

--- csharp/Monkey.Core/Parser/Parser.cs
+++ csharp/Monkey.Core/Parser/Parser.cs
@@ -21,12 +21,19 @@ public class Parser
         NextToken();
     }
 
+    /// <summary>
+    /// Advances both the current and peek token
+    /// </summary>
     private void NextToken()
     {
         _currentToken = _peekToken;
         _peekToken = _lexer.NextToken();
     }
 
+    /// <summary>
+    /// Iterates through all the tokens and turns all valid ones into statements or expressions
+    /// </summary>
+    /// <returns>A program with all valid statements</returns>
     public Ast.Program ParseProgram()
     {
         Ast.Program program = new Ast.Program(new List<Ast.Statement>());
@@ -46,17 +53,29 @@ public class Parser
         return program;
     }
 
+    /// <summary>
+    /// Returns private list of errors
+    /// </summary>
+    /// <returns>List of errors</returns>
     public List<string> Errors()
     {
         return _errors;
     }
 
+    /// <summary>
+    /// Adds an error if the type of peek token is not the same as input
+    /// </summary>
+    /// <param name="type">Expected type</param>
     private void PeekError(TokenType type)
     {
         string message = $"Expected next token to be {type}, got {_peekToken.Type}";
         _errors.Add(message);
     }
 
+    /// <summary>
+    /// Parses statements bases on the token
+    /// </summary>
+    /// <returns>A statement or null if it was an invalid statement</returns>
     private Ast.Statement? ParseStatement()
     {
         switch (_currentToken.Type)
@@ -107,19 +126,35 @@ public class Parser
         {
             NextToken();
         }
+
         return statement;
     }
 
+    /// <summary>
+    /// Checks if the current token is input type
+    /// </summary>
+    /// <param name="type">Type to be checked against</param>
+    /// <returns>True if the types match, false otherwise</returns>
     private bool IsCurrentTokenOfType(TokenType type)
     {
         return _currentToken.Type == type;
     }
 
+    /// <summary>
+    /// Checks if the peek token is input type
+    /// </summary>
+    /// <param name="type">Type to be checked against</param>
+    /// <returns>True if the types match, false otherwise</returns>
     private bool IsPeekTokenOfType(TokenType type)
     {
         return _peekToken.Type == type;
     }
 
+    /// <summary>
+    /// If the peek token is not of this type, then an error is added to the parsers list
+    /// </summary>
+    /// <param name="type">Expected type of peek token</param>
+    /// <returns>True if types match, false otherwise</returns>
     private bool ExpectPeekTokenOfType(TokenType type)
     {
         if (IsPeekTokenOfType(type))
