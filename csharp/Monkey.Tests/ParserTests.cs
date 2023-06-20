2dbb6e42db85f418b8e9aefaeb4b0a94fbaefb31
Traydr <Traydr@protonmail.com>
2023-06-20 21:40:14 +0300

    feat: add tests

--- csharp/Monkey.Tests/ParserTests.cs
+++ csharp/Monkey.Tests/ParserTests.cs
@@ -20,8 +20,7 @@ public class ParserTests
 
         if (program.Statements.Count != 3)
         {
-            Assert.Fail(
-                $"Expected 3 elements, got {program.Statements.Count}");
+            Assert.Fail($"Expected 3 elements, got {program.Statements.Count}");
         }
 
         List<string> expectedIdents = new List<string>() { "x", "y", "foobar" };
@@ -49,8 +48,7 @@ public class ParserTests
 
         if (program.Statements.Count != 3)
         {
-            Assert.Fail(
-                $"Expected 3 elements, got {program.Statements.Count}");
+            Assert.Fail($"Expected 3 elements, got {program.Statements.Count}");
         }
 
         foreach (var statement in program.Statements)
@@ -67,5 +65,43 @@ public class ParserTests
         }
 
         Assert.Pass();
+    }
+
+    [Test]
+    public void TestIdentifierExpression()
+    {
+        string input = "foobar;";
+        Lexer lexer = new Lexer(input);
+        Parser parser = new Parser(lexer);
+        Ast program = parser.ParseProgram();
+        TestUtils.CheckParserErrors(parser.Errors());
+
+        if (program.Statements.Count != 1)
+        {
+            Assert.Fail($"Expected 1 element, got {program.Statements.Count}");
+        }
+
+        if (program.Statements[0].GetType() != typeof(ExpressionStatement))
+        {
+            Assert.Fail($"Expected ExpressionStatement. got {program.Statements[0].GetType()}");
+        }
+
+        ExpressionStatement statement = (ExpressionStatement)program.Statements[0];
+        if (statement.Expression is not null &&
+            statement.Expression.GetType() != typeof(Identifier))
+        {
+            Assert.Fail($"Expected Identifier. got {program.Statements[0].GetType()}");
+        }
+
+        Identifier identifier = (Identifier)statement.Expression!;
+        if (identifier.Value != "foobar")
+        {
+            Assert.Fail($"Expected value foobar, got {identifier.Value}");
+        }
+
+        if (identifier.Token.Literal != "foobar")
+        {
+            Assert.Fail($"Expected literal foobar, got {identifier.Token.Literal}");
+        }
     }
 }
